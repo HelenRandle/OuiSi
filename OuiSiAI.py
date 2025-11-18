f28a79b6d2dc8b2f0920d1e939ad29ddcc900c95
JadynIinuma <jiinuma@hmc.edu>
2025-11-18 10:31:27 -0800

    import necessary models

--- OuiSiAI.py
+++ OuiSiAI.py
@@ -1,1 +1,14 @@
+import torch
+import torchvision
+import clip
+from PIL import Image
+from load_pretrained_models import load_model
+import basic_colormath
+from imagedominantcolor import DominantColor
+#use basic_colormath get_delta_e(rgb_a: Rgb, rgb_b: Rgb) -> float:
+
+
+shapeModel = load_model("resnet50_trained_on_SIN")
+textureModel = model = torchvision.models.resnet50(pretrained=False)
+semanticModel = clip.load() #cosine similarity
 
