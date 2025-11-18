13b715cecafa71e6584904910ef5c1fd53715b9e
JadynIinuma <jiinuma@hmc.edu>
2025-11-18 15:30:54 -0800

    make a window pop up

--- OuiSiGUI.py
+++ OuiSiGUI.py
@@ -1,1 +1,13 @@
+import tkinter as tk
+from PIL import Image, ImageTk
 
+root = tk.Tk()
+root.title("OuiSi!")
+root.minsize(1920,1080)
+
+img = Image.open("../OuiSiImages/ouisi-nature-099-inaê-guion-6-a.jpg")  
+img = ImageTk.PhotoImage(img)
+label = tk.Label(root, image=img)
+label.pack(expand=True)
+
+root.mainloop()
