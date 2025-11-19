9b3db3b41e60b35690235b818fa47b91beba94b4
JadynIinuma <jiinuma@hmc.edu>
2025-11-18 16:48:15 -0800

    crab.

--- OuiSiGUI.py
+++ OuiSiGUI.py
@@ -3,11 +3,27 @@ from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("OuiSi!")
-root.minsize(1920,1080)
-
-img = Image.open("../OuiSiImages/ouisi-nature-099-inaê-guion-6-a.jpg")  
+img = Image.open("ouisi-nature-099-inaê-guion-6-a.jpg")  
 img = ImageTk.PhotoImage(img)
-label = tk.Label(root, image=img)
-label.pack(expand=True)
+box = Image.open("box.png")  
+box = ImageTk.PhotoImage(box)
+
+for row in range(6):
+    for col in range(12):
+        tk.Button(
+                root,
+                image=box,
+                width=80,
+                height=80
+        ).grid(row=row, column=col, padx=1, pady=1)
+
+for column in range(0,10,2):
+    tk.Button(
+        root,
+        image=img,
+        width=160,
+        height=160
+    ).grid(row=6, column=column, padx=1, pady=1, columnspan=2, rowspan=2) 
+
 
 root.mainloop()
