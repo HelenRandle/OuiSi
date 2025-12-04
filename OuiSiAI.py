9785a6f0226a25d6ee98c5f62a0a012663f25b3d
JadynIinuma <jiinuma@hmc.edu>
2025-12-03 21:11:52 -0800

    finished the ai and made strides on the gui hooray!

--- OuiSiAI.py
+++ OuiSiAI.py
@@ -1,36 +1,71 @@
 import torch
 import torchvision
+from torch.nn import CosineSimilarity
+from torchvision.models.feature_extraction import create_feature_extractor
 import clip
 from PIL import Image
 from load_pretrained_models import load_model
+from colorthief import ColorThief
 import basic_colormath
-from imagedominantcolor import DominantColor
 from scipy.spatial.distance import cosine
 #use basic_colormath get_delta_e(rgb_a: Rgb, rgb_b: Rgb) -> float:
 
-
-shapeModel = load_model("resnet50_trained_on_SIN")
-textureModel = torchvision.models.resnet50(pretrained=False)
-semanticModel = clip.load() #cosine similarity
-
-print("shape: \n",shapeModel,"\n-------------------------")
-print("texture: \n",textureMOdel,"\n--------------------------")
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def compareTwo(im1, im2):
+# shapeModel = load_model("resnet50_trained_on_SIN")
+shapePremodel = torchvision.models.resnet50(pretrained=True)
+texturePremodel = torchvision.models.resnet50(pretrained=True)
+semanticModel, preprocess = clip.load("ViT-B/32", device=device)
+
+return_texture_nodes = {
+    "layer1.1.conv1": "layer1"
+}
+return_shape_nodes = {
+    "layer4.2.conv3": "layer4"
+}
+textureModel = create_feature_extractor(texturePremodel, return_nodes=return_texture_nodes)
+shapeModel = create_feature_extractor(shapePremodel, return_nodes=return_shape_nodes)
+
+
+def compareTwo(im1Path, im2Path):
     """
     Inputs: im1 and im2 strings, filepaths
-    Outputs:
+    Outputs: int that represents score made of a weighted average of the cossine similarities
     """
-    im1CLIP = preprocess(Image.open(im1)).unsqueeze(0).to(device)
-    im2CLIP = preprocess(Image.open(im2)).unsqueeze(0).to(device)
-    semanticDistance = shapeModel(im1CLIP, im2CLIP)
+    im1 = Image.open(im1Path)
+    im2 = Image.open(im2Path)
 
-    #TODO: image 1 and 2 need to get evaluated by texture and semantic model
-    textureDistance = 1 - cosine(vector1, vector2) #get vector from lower layer of neural network
-    shapeDistance = 1 - cosine(vector1, vector2) #get vector from last layer of neural network
+    #preprocessing
+    im1CLIP = preprocess(im1).unsqueeze(0).to(device)
+    im2CLIP = preprocess(im2).unsqueeze(0).to(device)
+    im1_features = semanticModel.encode_image(im1CLIP)
+    im2_features = semanticModel.encode_image(im2CLIP)
+    cossim = CosineSimilarity(dim=0, eps=1e-6)
+    semanticDistance = cossim(torch.flatten(im1_features), torch.flatten(im2_features)).item()*100
+    # print(semanticDistance.item())
+
+    #shape
+    im1Shape = torch.flatten(shapeModel(im1CLIP)['layer4'])
+    im2Shape = torch.flatten(shapeModel(im2CLIP)['layer4'])
+    shapeDistance = cossim(im1Shape, im2Shape).item()*100 #get vector from last layer of neural network
+    # print(shapeDistance.item())
+
+    #texture
+    im1Texture = torch.flatten(textureModel(im1CLIP)['layer1'])
+    im2Texture = torch.flatten(textureModel(im2CLIP)['layer1'])
+    textureDistance = cossim(im1Texture, im2Texture).item()*100 #get vector from lower layer of neural network
+    # print(textureDistance.item())
     
-    color1 = DominantColor(im1)
-    color2 = DominantColor(im2)
+    #color
+    color_thief1 = ColorThief(im1Path)
+    color_thief2 = ColorThief(im2Path)
+    color1 = color_thief1.get_color(quality=1)
+    color2 = color_thief2.get_color(quality=1)
     colorDistance =  basic_colormath.get_delta_e(color1, color2)
+
+    output = semanticDistance*0.2 + shapeDistance*0.52 + textureDistance*0.09 + colorDistance*0.18
+    output = output/4
+    return output
+
+rootPath = "../Downloads/OuiSiOG"
+compareTwo(str(rootPath+"/ouisi-nature-002.jpg"),str(rootPath+"/ouisi-nature-005.jpg"))
--- OuiSiGUI.py
+++ OuiSiGUI.py
@@ -1,35 +1,101 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import random
 import numpy as np
+from OuiSiAI import compareTwo
 
 root = tk.Tk()
 root.title("OuiSi!")
-box = Image.open("box.png")  
+pathRoot = "../Downloads/OuiSiOG/"
+box = Image.open(pathRoot+"box.png")  
 box = ImageTk.PhotoImage(box)
-cardDeck = range(1,210)
-cardDeck = np.random.shuffle(cardDeck)
+cardDeck = list(range(1,210))
+random.shuffle(cardDeck)
+matchDict = {}
+openImages = {}
+board = np.zeros((6,12))
 
 for row in range(6):
-    filepath = "../ouisi-nature-"+str(0)*3-len(str(cardDeck[0]))+str(cardDeck[0])
-    img = Image.open(filepath)
-    img = ImageTk.PhotoImage(img)
-    cardDeck.pop()  
-    
     for col in range(12):
+        if (row==2 and col==5):
+            filepath = pathRoot+"ouisi-nature-"+str(0)*(3-len(str(cardDeck[0])))+str(cardDeck[0])+".jpg"
+            openImages["imgstarter"] = Image.open(filepath)
+            openImages["imgstarter"] = ImageTk.PhotoImage(openImages["imgstarter"])
+            im = openImages["imgstarter"]
+            cardDeck = cardDeck[1:]
+        else:
+            im=box,
         tk.Button(
                 root,
-                image=box,
+                image = im,
                 width=80,
                 height=80
         ).grid(row=row, column=col, padx=1, pady=1)
 
 for column in range(0,10,2):
+    filepath = pathRoot+"ouisi-nature-"+str(0)*(3-len(str(cardDeck[0])))+str(cardDeck[0])+".jpg"
+    openImages["img"+str(column)] = Image.open(filepath)
+    openImages["img"+str(column)] = ImageTk.PhotoImage(openImages["img"+str(column)])
+    cardDeck = cardDeck[1:]
     tk.Button(
         root,
-        image=img,
+        image=openImages["img"+str(column)],
         width=160,
         height=160
     ).grid(row=6, column=column, padx=1, pady=1, columnspan=2, rowspan=2) 
 
+aiHand = cardDeck[:6]
+cardDeck = cardDeck[6:]
+
+def score(card, match):
+    cardPath = pathRoot+"ouisi-nature-"+str(0)*(3-len(str(card)))+str(card)+".jpg"
+    matchPath = pathRoot+"ouisi-nature-"+str(0)*(3-len(str(match)))+str(match)+".jpg"
+    if card < match:
+        if str(card)+str(match) in matchDict:
+            return matchDict[str(card)+str(match)]
+        else:
+            score = compareTwo(cardPath, matchPath)
+            matchDict[str(card)+str(match)] = score
+            return score
+    else:
+        if str(match)+str(card) in matchDict:
+            return matchDict[str(match)+str(card)]
+        else:
+            score = compareTwo(cardPath, matchPath)
+            matchDict[str(match)+str(card)] = score
+            return score
+
+
+
+def playerTurn():
+    """
+    let player click own card and place on board
+    update board 2d array
+    """
+    #play card
+    #dont forget to draw card
+
+def getAllOpenCards():
+    """
+    input: none
+    output: numbers of all images on board
+    """
+    return 0
+
+def aiTurn():
+    bestScore = (0, 0, 0) #(card matched with, card played, score)
+    boardCards = getAllOpenCards()
+    for card in aiHand:
+        for match in boardCards:
+            newScore = score(card, match)
+            if newScore > bestScore[2]:
+                bestScore[0] = match
+                bestScore[1] = card
+                bestScore[2] = newScore
+                
+    #play card, draw card
+
+
 
 root.mainloop()
+#figure out how to take turns :D
